f1c86275501635cd8b0d82fabf88d7010ce952e0
Tomasz Wołyniec <t.wolyniec@tlen.pl>
2020-11-03 09:29:15 +0100

    3,11

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function themeSwitch() {
+    const switchState = document.getElementById("checkbox").checked;
+    const elements = document.querySelectorAll(".dark");
+    console.log(switchState);
     
 }
